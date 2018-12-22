02d0eeee4712f64095e34ba4df2e9db32e463c4b
easywaldo <easywaldoji@gmail.com>
2018-12-23 01:18:43 +0900

    input 상태관리
    handleCrate 와 handleChange 를 이용한 상태관리를 실습한다

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import PhoneForm from './components/PhoneForm';
 
 var productInfo = {
   id: 999,
@@ -8,11 +9,26 @@ var productInfo = {
 }
 
 class App extends Component {
+  state = {
+    information: [],
+  }
+  
+  handleCreate = (data) =>{
+    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat(data),
+    })
+  }
+
+
 
   render() {
     return (
       <div className="App">
-        <Counter product={productInfo} />
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
+        {/* <Counter product={productInfo} /> */}
       </div>
     );
   }
---
+++ src/components/PhoneForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+
+    state = {
+        name: "",
+        phone: "",
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        //e.preventDefalut();
+        e.nativeEvent.stopImmediatePropagation();
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: "",
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <input 
+                    placeholder="please enter your name"
+                    name="name"
+                    onChange={this.handleChange}
+                    value={this.state.name} />
+                <input
+                    placeholder="plese enter your phone number"
+                    name="phone"
+                    onChange={this.handleChange}
+                    value={this.state.phone} />
+                <div>
+
+                <button type="submit">등록</button>
+                {/* {this.state.name}
+                {this.state.phone} */}
+                </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default PhoneForm;
